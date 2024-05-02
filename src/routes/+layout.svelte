d0bbf3a77968b63bd1259df0f3025d7392523160
uimaxbai <uimax.bai@gmail.com>
2024-05-02 17:42:33 +0100

    f

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,62 @@
+<style lang="scss">
+    @font-face {
+        font-family: 'Helvetica Neue';
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue-Light.eot?#iefix') format('embedded-opentype'),
+            url('fonts/HelveticaNeue/woff2/hinted-HelveticaNeue-Light.woff2') format('woff2'),
+            url('fonts/HelveticaNeue/woff/hinted-HelveticaNeue-Light.woff') format('woff'),
+            url('fonts/HelveticaNeue/ttf/hinted-HelveticaNeue-Light.ttf') format('truetype');
+        font-weight: 300;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Helvetica Neue';
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue-Bold.eot');
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue-Bold.eot?#iefix') format('embedded-opentype'),
+            url('fonts/HelveticaNeue/woff2/hinted-HelveticaNeue-Bold.woff2') format('woff2'),
+            url('fonts/HelveticaNeue/woff/hinted-HelveticaNeue-Bold.woff') format('woff'),
+            url('fonts/HelveticaNeue/ttf/hinted-HelveticaNeue-Bold.ttf') format('truetype');
+        font-weight: bold;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Helvetica Neue';
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue.eot');
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue.eot?#iefix') format('embedded-opentype'),
+            url('fonts/HelveticaNeue/woff2/hinted-HelveticaNeue.woff2') format('woff2'),
+            url('fonts/HelveticaNeue/woff/hinted-HelveticaNeue.woff') format('woff'),
+            url('fonts/HelveticaNeue/ttf/hinted-HelveticaNeue.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Helvetica Neue';
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue-Italic.eot');
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue-Italic.eot?#iefix') format('embedded-opentype'),
+            url('fonts/HelveticaNeue/woff2/hinted-HelveticaNeue-Italic.woff2') format('woff2'),
+            url('fonts/HelveticaNeue/woff/hinted-HelveticaNeue-Italic.woff') format('woff'),
+            url('fonts/HelveticaNeue/ttf/hinted-HelveticaNeue-Italic.ttf') format('truetype');
+        font-weight: normal;
+        font-style: italic;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Helvetica Neue';
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue-BoldItalic.eot');
+        src: url('fonts/HelveticaNeue/eot/hinted-HelveticaNeue-BoldItalic.eot?#iefix') format('embedded-opentype'),
+            url('fonts/HelveticaNeue/woff2/hinted-HelveticaNeue-BoldItalic.woff2') format('woff2'),
+            url('fonts/HelveticaNeue/woff/hinted-HelveticaNeue-BoldItalic.woff') format('woff'),
+            url('fonts/HelveticaNeue/ttf/hinted-HelveticaNeue-BoldItalic.ttf') format('truetype');
+        font-weight: bold;
+        font-style: italic;
+        font-display: swap;
+    }
+</style>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <style lang="scss">
+    $font: 'Helvetica Neue', 'Helvetica', Arial, sans-serif;
     :global(html, body, body .mainDiv) {
         width: 100%;
         height: 100%;
         padding: 0;
         margin: 0;
-        font-family: 
+        font-family: $font;
     }
     .search {
         display: flex;
@@ -13,31 +14,71 @@
             padding: 1em;
             border-radius: .5em;
             border: 1px solid #ccc;
+            font-family: $font;
+        }
+        ul {
+            list-style-type: none;
         }
     }
 </style>
 
 <div class="search">
     <div class="from">
-        <input type="text" placeholder="From...">
+        <input type="text" bind:value={fromInput} placeholder="From..." on:input={() => updateSearch(false)}>
+        <ul>
+            {#if resultsFrom}
+                {#each Object.keys(resultsFrom) as item}
+                <li><button>{item}</button></li>
+                {/each}
+            {/if}
+        </ul>
     </div>
     <div class="to">
-        <input type="text" placeholder="To...">
+        <input type="text" bind:value={toInput} placeholder="To..." on:input={() => updateSearch(true)}>
+        <ul>
+            {#if resultsTo}
+                {#each Object.keys(resultsTo) as item}
+                    <li><button>{item}</button></li>
+                {/each}
+            {/if}
+        </ul>
     </div>
 </div>
 
 <script lang="ts">
     import { onMount } from 'svelte';
-    const searchWikipedia = async (q: String) => {
-        const response = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=${q}`, {
-        });
-        let json = await response.json();
-        return json;
+
+    let resultsTo: Record<string, string> = {};
+    let resultsFrom: Record<string, string> = {};
+    let toInput = "";
+    let fromInput = "";
+    // TODO: set from and to using the buttons in the input
+    let from = "";
+    let to = "";
+
+
+    const searchWikipedia = async (q: string) => {
+        const response = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${q}&origin=*`);
+        let resJson = await response.json();
+        return resJson;
     };
 
-    onMount(() => {
-        searchWikipedia("pizza").then(d => {
-            console.log(d);
-        })
-    });
+    const updateSearch = (to: boolean) => {
+        if (to === false) { // from
+            searchWikipedia(fromInput).then(data => {
+                resultsFrom = {};
+                (data[1]).forEach((el: string, i: number) => {
+                    resultsFrom[el] = data[3][i];
+                })
+            })
+        }
+        else if (to === true) { // to
+            searchWikipedia(toInput).then(data => {
+                resultsTo = {};
+                (data[1]).forEach((el: string, i: number) => {
+                    resultsTo[el] = data[3][i];
+                })
+            })
+        }
+    }
 </script>
