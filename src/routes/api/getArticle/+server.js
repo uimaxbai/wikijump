0671f1a2c74b252b774de43677158f28fee4cf94
uimaxbai <uimax.bai@gmail.com>
2024-05-22 11:45:27 +0100

    yessss!

---
+++ src/routes/api/getArticle/+server.js
@@ -0,0 +1,58 @@
+import { error, json } from '@sveltejs/kit';
+import sanitiseHtml from 'sanitize-html';
+import { JSDOM } from "jsdom";
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
+    const name = url.searchParams.get('n') || "";
+    if (name === "") {
+        error(400, "No Wikipedia article specified.");
+    }
+    var linktoFetch = `https://en.wikipedia.org/w/api.php?action=parse&page=${name}&prop=text&formatversion=2&origin=*&format=json`;
+    const response = await fetch(linktoFetch);
+    let resJson = await response.json();
+    // console.log(resJson)
+    try {
+        if (typeof resJson.error !== undefined) {
+            if (resJson.error.code === "missingtitle") {
+                error(404, JSON.stringify({
+                    status: 404,
+                    body: `Page ${name} does not exist on Wikipedia.` 
+                }))
+            }
+            else {
+                error(500, JSON.stringify({
+                    status: 500,
+                    body: `Unknown error occured.`,
+                    error: resJson
+                }))
+            }
+        }
+    }
+    catch (e) {
+        // idk why this works, but maybe it's because resJson.error doesn't exist?????
+    }
+    console.log(1);
+    let article = resJson["parse"]["text"];
+
+    sanitiseHtml(article);
+    const htmlDoc = new JSDOM(article);
+    htmlDoc.window.document.querySelectorAll('.mw-editsection').forEach(e => e.remove()); // get rid of edit buttons
+    // return htmlDoc.body.innerHTML
+    // remove external links
+    htmlDoc.window.document.querySelector(".external").style.color = "#f00";
+    htmlDoc.window.document.querySelector(".external")?.setAttribute("href", "");
+    
+    let data = {
+        status: 200,
+        body: htmlDoc.window.document.body.innerHTML
+    }
+
+
+    return json(data);
+}
+
+/** @type {import('./$types').RequestHandler} */
+export async function fallback({ request }) {
+    error(405, "Method not allowed. Use a GET request instead.");
+}
